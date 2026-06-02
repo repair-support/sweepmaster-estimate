d4cc2eae926d994b8fc3f5edfd299c792108fb5e
kubota <your-email@example.com>
2026-06-02 14:17:49 +0900

    Update estimate app 2026-06-02 14:17:49

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.SWEEPMASTER_CONFIG = {
   // settings.html で保存したURLがある場合は、そちらが優先されます。
   sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7fJz7VQMdJkZ65c6vM1_byxhHj8I9delB5j_yehapRvmZUuJF2-FenxFKZNm7MUj4pWDl_FZ6RrS/pub?gid=0&single=true&output=csv",
   // 管理画面から料金を保存するためのGoogle Apps ScriptウェブアプリURLです。
-  adminApiUrl: ""
+  adminApiUrl: "https://script.google.com/macros/s/AKfycbxmEIoMJFK1PggWkg2UGvVWXoxmqctbnO2wIZjY1LTGaQ2VyUeDiLEYQ_AYaWuRVPtsjg/exec"
 };
