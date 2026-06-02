006c6863b70538a36dc29cbbf577e984ed99b313
kubota <your-email@example.com>
2026-06-02 12:46:50 +0900

    Update estimate app 2026-06-02 12:46:50

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SWEEPMASTER_CONFIG = {
   // Googleスプレッドシートを「ウェブに公開」したCSV URLを設定できます。
   // settings.html で保存したURLがある場合は、そちらが優先されます。
-  sheetCsvUrl: ""
+  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7fJz7VQMdJkZ65c6vM1_byxhHj8I9delB5j_yehapRvmZUuJF2-FenxFKZNm7MUj4pWDl_FZ6RrS/pub?gid=0&single=true&output=csv"
 };
